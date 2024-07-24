2d8949c7e3f30d77a793fda4427e428f4a453d93
Anshuman Bhardwaj <64587347+A-nshuman@users.noreply.github.com>
2024-07-24 15:11:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from textblob import TextBlob
 from transformers import pipeline
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pyttsx3
 
-st.set_page_config(page_title="EmoSent", page_icon=":smiley:", layout="wide")
+st.set_page_config(page_title="EmoSent", page_icon=":smiley:", layout="centered")
 
 st.title("Sentiment and Emotion Analyzer")
 
@@ -16,54 +17,70 @@ if st.button("Analyze"):
         senti = blob.sentiment.polarity
 
         if senti < 0:
-            sentiment = 'negative'
+            sentiment = 'Negative'
             col = "#FF6666"
         elif senti > 0:
-            sentiment = 'positive'
+            sentiment = 'Positive'
             col = "#b3d23e"
         else:
-            sentiment = 'neutral'
+            sentiment = 'Neutral'
             col = "#d3d3d3"
 
+        # Emotions
         classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
         model_outputs = classifier(user_input)
+        emotion_score = model_outputs[0][0]['score']
+        emotion = model_outputs[0][0]['label']
 
-        high_score_1 = 0
-        emotion_1 = ''
-        for output in model_outputs[0]:
-            if output['score'] > high_score_1:
-                high_score_1 = output['score']
-                emotion_1 = output['label']
+        # Summary
+        summarizer = pipeline("summarization", model="philschmid/bart-large-cnn-samsum")
+        summary = summarizer(user_input, max_length=100, min_length=30, do_sample=False)[0]['summary_text']
 
         st.subheader("Analysis Results")
-        st.write(f"**Sentiment:** {sentiment.capitalize()}")
-        st.write(f"**Emotion:** {emotion_1.capitalize()}")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write(f"**Sentiment:** {sentiment}")
+        with col2:
+            st.write(f"**Emotion:** {emotion.capitalize()}")
 
-        fig, ax = plt.subplots(figsize=(10, 2))
-        ax.barh(y=0, width=senti, height=1, color=col)
+        col3, col4 = st.columns(2)
+        with col3:
+            st.subheader("Summary")
+            st.write(f"{summary}")
+        
+        with col4:
+            st.subheader("Graphs")
+            # Graph 1 : Sentiment
+            fig, ax = plt.subplots(figsize=(10, 0.5))
+            ax.barh(y=0, width=senti, height=1, color=col)
 
-        ax.set_xlim(-1, 1)
-        ax.set_ylim(-0.5, 0.5)
-        ax.grid(True)
-        ax.axvline(0, color='black', linewidth=0.5)
-        ax.set_title('Graphical Representation of Sentiment')
-        ax.set_xlabel('Sentiment Value')
-        ax.get_yaxis().set_visible(False)
-        ax.text(senti, 0, f'{senti:.2f}', color='black', va='center', ha='left' if senti < 0 else 'right')
+            ax.set_xlim(-1, 1)
+            ax.set_ylim(-0.5, 0.5)
+            ax.grid(True)
+            ax.axvline(0, color='black', linewidth=0.5)
+            ax.set_xlabel(f'Sentiment = {sentiment}')
+            ax.get_yaxis().set_visible(False)
+            ax.text(senti, 0, f'{senti:.2f}', color='black', va='center', rotation=90 if senti > 0 else 270, ha='left' if senti < 0 else 'right')
+            plt.title("Senitments Value Representation")
+            st.pyplot(fig)
 
-        st.pyplot(fig)
+            # Graph 2 : Emotion
+            emotion_scores = {output['label']: output['score'] for output in model_outputs[0]}
+            graphEmotions = list(emotion_scores.keys())[:5]
+            graphValue = list(emotion_scores.values())[:5]
 
-        st.subheader("Emotion Distribution")
-        emotion_scores = {output['label']: output['score'] for output in model_outputs[0]}
-        emotion_labels = list(emotion_scores.keys())
-        emotion_values = list(emotion_scores.values())
+            plt.figure(figsize=(12, 6))
+            sns.barplot(x=graphValue, y=graphEmotions, palette='viridis')
+            plt.xlabel('Accuracy')
+            plt.ylabel('Top 5 Emotions')
+            plt.title('Emotion Distribution')
+            st.pyplot(plt)
 
-        plt.figure(figsize=(12, 6))
-        sns.barplot(x=emotion_values, y=emotion_labels, palette='viridis')
-        plt.xlabel('Score')
-        plt.ylabel('Emotion')
-        plt.title('Emotion Distribution')
+        # Text to speech
+        tts_engine = pyttsx3.init()
+        tts_engine.say(f'The above paragraph is {sentiment}')
+        tts_engine.say(f'The emotion in the above paragraph is {emotion}')
+        tts_engine.runAndWait()
 
-        st.pyplot(plt)
     else:
         st.error("Please enter some text to analyze.")
